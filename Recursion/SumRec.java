5f04fbdfaf0578b3a1c6777dc9d3699ff7071ed5
Suman Ranjan <sumanranjan17291729@gmail.com>
2025-02-26 22:13:29 +0530

    bit

--- Recursion/SumRec.java
+++ Recursion/SumRec.java
@@ -9,8 +9,8 @@ public class SumRec {
 
     static int sum(int n) {
 
-        if (n <= 1) {
-            return 1;
+        if (n == 0) {
+            return 0;
         }
         
         return n + sum(n - 1);

9cf2dac4916ed2ccac8a7c2924fbb30ef238b4f7
Suman Ranjan <sumanranjan17291729@gmail.com>
2025-03-24 11:56:50 +0530

    generic

--- OPPS/Generics/CustomArrayList.java
+++ OPPS/Generics/CustomArrayList.java
@@ -1,4 +1,73 @@
 package OPPS.Generics;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CustomArrayList {
+
+    private int [] data;
+    private static final int DEFAULT_SIZE = 10;
+    private int size = 0;
+
+    public CustomArrayList() {
+        this.data = new int[DEFAULT_SIZE];
+    }
+
+    public  void add(int num){
+        if(isFull()){
+            resize();
+        }
+
+        data[size++] = num;
+    }
+
+    private void resize() {
+        int [] temp = new int[data.length * 2];
+
+        System.arraycopy(data, 0, temp, 0, data.length);
+
+        data = temp;
+    }
+
+    private boolean isFull() {
+        return size == data.length;
+    }
+
+    public int remove(){
+        return data[--size];
+    }
+
+    public int get(int index){
+        return data[index];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void set(int index , int val){
+        data[index] = val;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomArrayList{" +
+                "data=" + Arrays.toString(data) +
+                ", size=" + size +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        CustomArrayList list = new CustomArrayList();
+
+//        list.add(3);
+//        list.add(4);
+//        list.add(5);
+
+        for (int i = 0; i < 14; i++) {
+            list.add(2*i);
+        }
+
+        System.out.println(list);
+    }
 }
--- OPPS/Generics/CustomGenArrayList.java
+++ OPPS/Generics/CustomGenArrayList.java
@@ -1,4 +1,78 @@
 package OPPS.Generics;
 
-public class CustomGenArrayList {
+import OPPS.Properties.Polymorphism.Number;
+
+import java.util.Arrays;
+
+//here t should either number or its subclasses
+// T extend Number upperbound wildcard
+public class CustomGenArrayList<T> {
+
+    private Object[] data;
+    private static final int DEFAULT_SIZE = 10;
+    private int size = 0;
+
+    public CustomGenArrayList() {
+        data = new Object[DEFAULT_SIZE];
+    }
+
+    //This is wild card
+//    public void getList(List<? extends Number>list){
+//        //do something
+//    }
+
+    public void add(T num) {
+        if (isFull()) {
+            resize();
+        }
+        data[size++] = num;
+    }
+
+    private void resize() {
+        Object[] temp = new Object[data.length * 2];
+        System.arraycopy(data, 0, temp, 0, data.length);
+        data = temp;
+    }
+
+    private boolean isFull() {
+        return size == data.length;
+    }
+
+    public T remove() {
+        T removed = (T) data[--size];
+        return removed;
+    }
+
+    public T get(int index) {
+        return (T) data[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void set(int index, T val) {
+        data[index] = val;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomArrayList{" +
+                "data=" + Arrays.toString(data) +
+                ", size=" + size +
+                '}';
+    }
+    public static void main(String[] args) {
+        CustomGenArrayList<Integer>list = new CustomGenArrayList<>();
+
+        for (int i = 0; i < 14; i++) {
+            list.add(i * 2);
+        }
+
+        System.out.println(list);
+        list.remove();
+        System.out.println(list);
+        list.add(100);
+        System.out.println(list);
+    }
 }
